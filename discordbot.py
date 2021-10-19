1e064323f6f6176928b2ee54e72650a68ed050a1
ru-nahuran <88400288+ru-nahuran@users.noreply.github.com>
2021-10-19 22:16:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -146,7 +146,7 @@ async def on_voice_state_update(member, before, after):
             await client.change_presence(activity=discord.Game(name=presence))
         else:
             if member.guild.voice_client is None:
-                #await asyncio.sleep(0.5)
+                await asyncio.sleep(0.5)
                 #await after.channel.connect()
             else:
                 if member.guild.voice_client.channel is after.channel:
