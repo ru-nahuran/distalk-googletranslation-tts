fe3d0f6ce89a86ef8646eccf9ca70ac11b61266d
ru-nahuran <88400288+ru-nahuran@users.noreply.github.com>
2022-01-01 02:42:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -135,6 +135,8 @@ async def on_message(message):
                 text = re.sub(pattern, '、かずは', text)
                 pattern = r'申鶴'
                 text = re.sub(pattern, '、しんかく', text)
+                pattern = r'胡桃'
+                text = re.sub(pattern, '、ふぅたお', text)
                 if text[-1:] == 'w' or text[-1:] == 'W' or text[-1:] == 'ｗ' or text[-1:] == 'W':
                     while text[-2:-1] == 'w' or text[-2:-1] == 'W' or text[-2:-1] == 'ｗ' or text[-2:-1] == 'W':
                         text = text[:-1]
