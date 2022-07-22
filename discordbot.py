a375dd2213d722a124e653c9144164f44d219891
ru-nahuran <88400288+ru-nahuran@users.noreply.github.com>
2022-07-22 14:38:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -100,13 +100,13 @@ async def on_message(message):
                         #text = text.replace(char, emoji_dataset[char]['short_name'])                
                 #pattern = r'<@(\d+)>'
                 #match = re.findall(pattern, text)
-                 user_id in match:
+                for user_id in match:
                     user = await client.fetch_user(user_id)
                     user_name = f'、{user.name}へのメンション、'
                     text = re.sub(f'<@{user_id}>', user_name, text)
                 pattern = r'<@&(\d+)>'
                 match = re.findall(pattern, text)
-                for role_id in match:
+                role_id in match
                     role = message.guild.get_role(int(role_id))
                     role_name = f'、{role.name}へのメンション、'
                     text = re.sub(f'<@&{role_id}>', role_name, text)
